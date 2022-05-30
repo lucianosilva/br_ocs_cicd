138a7b22fff645f49d8d3c5b160eb507e0874c2e
Luciano Silva Carmo <lcarmo2701@gmail.com>
2022-05-30 17:40:46 -0300

    Terraform Update

--- main.tf
+++ main.tf
@@ -8,8 +8,11 @@ terraform {
 
 provider "oci" {
   auth = "InstancePrincipal"
-  region = "${var.region}"
-#  config_file_profile = "HardToDie"
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa7dtj7joj6p4fwbyb72nyguyegbggapbnhmyko7inzmlijk4n7eka"
+  user_ocid = "ocid1.user.oc1..aaaaaaaazbtucrdkg6k5icecpqh5omduocrmdcdkeahgxszdk5pn4mnip3za"
+  fingerprint = "ae:e2:c0:de:86:36:04:2b:b4:4f:a3:b0:63:2c:67:e2"
+  private_key_path = "~/.oci/key.pem"
+  region = sa-saopaulo-1"
 }
 
 resource "oci_core_vcn" "internal" {
