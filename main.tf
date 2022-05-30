93c5e8a2e83efef2221ed176c21a2eb241285e87
Luciano Silva Carmo <lcarmo2701@gmail.com>
2022-05-30 17:17:24 -0300

    Terraform Update

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+    }
+  }
+}
+
+provider "oci" {
+  region              = "sa-saopaulo-1"
+  auth                = "SecurityToken"
+  config_file_profile = "HardToDie"
+}
+
+resource "oci_core_vcn" "internal" {
+  dns_label      = "internal"
+  cidr_block     = "10.0.0.0/16"
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaa6cu4btpjaxboimomt63c6g5glewevoqksku4ggkpt3be4c5lo5ga"
+  display_name   = "vcn-20220211-1442"
+}
