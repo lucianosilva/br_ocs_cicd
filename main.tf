c447355f447e3967839a594d3bb0e0ee5a7e5a0d
Luciano Silva Carmo <lcarmo2701@gmail.com>
2022-05-31 13:53:43 -0300

    VCN Fix

--- main.tf
+++ main.tf
@@ -40,9 +40,9 @@ terraform {
 
 resource "oci_core_vcn" "vcn" {
   cidr_blocks    = ["10.0.0.0/16","11.0.0.0/16"]
-  dns_label      = "vcn_tf"
+  dns_label      = "vcn"
   compartment_id = var.compartment_ocid
-  display_name   = "vcn_tf"
+  display_name   = "vcn"
 }
 
 output "vcn_id" {
