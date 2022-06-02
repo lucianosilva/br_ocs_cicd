d4316847ab5ab8a3a1e29da28ca0b23bfd4f5aa3
Luciano Silva Carmo <lcarmo2701@gmail.com>
2022-06-02 16:57:55 -0300

    Try to fix TF

--- main.tf
+++ main.tf
@@ -38,17 +38,13 @@ terraform {
   }
 }
 
-resource "oci_core_vcn" "vcn" {
+resource "oci_core_vcn" "vcn2" {
   cidr_blocks    = ["10.0.0.0/16","11.0.0.0/16"]
-  dns_label      = "vcn"
+  dns_label      = "vcn2"
   compartment_id = var.compartment_ocid
-  display_name   = "vcn"
+  display_name   = "vcn2"
 }
 
-# Output
-
-output "getCompartmentId" { value = var.compartment_ocid }
-
 output "vcn_id" {
-  value = oci_core_vcn.vcn.id
+  value = oci_core_vcn.vcn2.id
 }
